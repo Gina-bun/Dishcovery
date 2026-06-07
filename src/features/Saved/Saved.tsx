3c41a55f2c019cf7a06244a0b79d953ac15777e9
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-06-07 11:19:39 +0000

    feat: add saved recipe page

--- src/features/Saved/Saved.tsx
+++ src/features/Saved/Saved.tsx
@@ -1,14 +1,31 @@
-
-
+import { MealCard } from "../../components/MealCard/MealCard"
+import { useSavedMeals } from "../../context/SavedMealsContext"
 
 
 
 
 export function Saved(){
+    const {savedMeals} = useSavedMeals()
 
     return (
         <>
-        <h1>Saved Page</h1>
+        <h1>Saved Recipe</h1>
+        {savedMeals.length === 0 ? (
+            <div>
+                <p>No saved recipes yet.</p>
+                <p>Go explore and save something delicious.</p>
+            </div>
+        ) : 
+        (
+            <div>
+                {
+                    savedMeals.map((meal) => (
+                        <MealCard key={meal.idMeal} meal={meal} />
+                    ))
+                }
+            </div>
+        )
+        }
         </>
     )
 }
