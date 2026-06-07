1e60181dd23629a742fe6864735edb1199a00f93
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-06-07 00:40:01 +0000

    feat: add categrories and meal list pages

--- src/features/Categories/Categories.tsx
+++ src/features/Categories/Categories.tsx
@@ -1,13 +1,38 @@
+import { useNavigate } from "react-router"
+import type { Category } from "../../types/types"
+import useFetch from "../../hooks/useFetch"
 
 
 
-
+interface CategoriesResponse {
+    categories: Category[]
+}
 
 export function Categories(){
+    const navigate = useNavigate()
+    const {data, loading, error} = useFetch<CategoriesResponse>(
+        `https://www.themealdb.com/api/json/v1/1/categories.php`
+    )
+
+    if (loading) return <p>Loading categories...</p>
+    if(error) return <p>Something went wrong: {error}</p>
 
     return (
         <>
-        <h1>Categories Page</h1>
+        <h1>All Categories</h1>
+        <div className="grid grid-cols-3 gap-3">
+            {data?.categories.map((category) => (
+                <div
+                key={category.idCategory}
+                onClick={() => navigate(`/meals?category=${category.strCategory}`)}
+                style={{cursor: "pointer"}}
+                >
+                    <img src={category.strCategoryThumb} alt={category.strCategory} />
+                    <h2>{category.strCategory}</h2>
+                    <p>{category.strCategoryDescription.slice(0, 80)}...</p>
+                </div>
+            ))}
+        </div>
         </>
     )
 }
--- src/features/MealList/MealList.tsx
+++ src/features/MealList/MealList.tsx
@@ -1,13 +1,45 @@
 
+import { useSearchParams } from "react-router"
+import type { Meal } from "../../types/types"
+import useFetch from "../../hooks/useFetch"
+import { MealCard } from "../../components/MealCard/MealCard"
 
-
+interface MealListResponse {
+    meals: Meal[] | null
+}
 
 
 export function MealList(){
+    const [searchParams] = useSearchParams()
+    const category = searchParams.get("category")
+    const search = searchParams.get("search")
+
+    const url = category
+    ?  `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+    : search
+    ? `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    : null
+    const {data, loading, error} = useFetch<MealListResponse>(url) 
+
+    const title = category ? category : search ? `Results for "${search}"` : "Meals"
+
+    if (loading) return <p>Loading meals...</p>
+    if (error) return <p>Something went wrong: {error}</p>
 
     return (
         <>
-        <h1>MealList Page</h1>
+        <h1>{title}</h1>
+        {
+            !data?.meals ? (
+                <p>No meals found. try something else.</p>
+            ) : (
+                <div>
+                    {data.meals.map((meal) => (
+                        <MealCard key={meal.idMeal} meal={meal} />
+                    ))}
+                </div>
+            )
+        }
         </>
     )
 }
