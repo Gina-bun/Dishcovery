b329b185d2593012e870f82585d1f08cd1323c6e
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-06-07 16:49:03 +0000

    style: add style for meal category page

--- src/features/Categories/Categories.tsx
+++ src/features/Categories/Categories.tsx
@@ -25,17 +25,18 @@ export function Categories() {
 
   return (
     <>
-      <h1>All Categories</h1>
-      <div className="grid grid-cols-3 gap-3">
+      <h1 className="font-medium py-3 pb-5 text-rose-600 sm:text-xl">All Categories</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 pb-12">
         {data?.categories.map((category) => (
           <div
             key={category.idCategory}
             onClick={() => navigate(`/meals?category=${category.strCategory}`)}
             style={{ cursor: "pointer" }}
+            className="meal-category flex flex-col bg-gray-200 p-2"
           >
-            <img src={category.strCategoryThumb} alt={category.strCategory} />
-            <h2>{category.strCategory}</h2>
-            <p>{category.strCategoryDescription.slice(0, 80)}...</p>
+            <img className="m-auto" src={category.strCategoryThumb} alt={category.strCategory} />
+            <h2 className="font-medium py-1 sm:py-3 text-mauve-800">{category.strCategory}</h2>
+            <p className="text-sm">{category.strCategoryDescription.slice(0, 54)}...</p>
           </div>
         ))}
       </div>
